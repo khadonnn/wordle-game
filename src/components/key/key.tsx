694332dce4603f027bb691cc6ca7acdff4d4680f
khadon <nguyendinhdongkha@gmail.com>
2025-04-24 09:16:49 +0700

    add keyboard down

--- src/components/key/key.tsx
+++ src/components/key/key.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import './key.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
-import { incPos, setBoard } from '@/redux/boardSlice';
+
 interface IProps {
     letter: string;
+    onClick?: (letter: string) => void;
 }
-const Key: React.FC<IProps> = (props) => {
-    const { letter } = props;
-    const board = useSelector((state: RootState) => state.board.board);
-    const position = useSelector((state: RootState) => state.board.pos);
-    const dispatch = useDispatch();
-    const chooseLetter = () => {
-        if (position >= 30) return;
-        const newBoard = [...board];
-        newBoard[position] = letter;
-        dispatch(setBoard(newBoard));
-        dispatch(incPos());
-    };
+
+const Key: React.FC<IProps> = ({ letter, onClick }) => {
     return (
-        <div className='letter' onClick={chooseLetter}>
+        <div className='letter' onClick={() => onClick?.(letter)}>
             {letter}
         </div>
     );
 };
+
 export default Key;
--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './keyboard.css';
 import Key from '@/components/key/key';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
-import { decPos, setBoard } from '@/redux/boardSlice';
+import { decPos, incPos, setBoard } from '@/redux/boardSlice';
+
 const Keyboard: React.FC = () => {
     const rows: string[] = [
         'q w e r t y u i o p',
@@ -13,33 +14,64 @@ const Keyboard: React.FC = () => {
     const dispatch = useDispatch();
     const position = useSelector((state: RootState) => state.board.pos);
     const board = useSelector((state: RootState) => state.board.board);
-    const clickBack = () => {
-        if (position <= 0) return;
-        const newBoard = [...board];
-        newBoard[position - 1] = '';
-        dispatch(setBoard(newBoard));
-        dispatch(decPos());
+
+    const handleKeyInput = (key: string) => {
+        const letter = key.toLowerCase();
+
+        if (letter === 'backspace') {
+            if (position <= 0) return;
+            const newBoard = [...board];
+            newBoard[position - 1] = '';
+            dispatch(setBoard(newBoard));
+            dispatch(decPos());
+        } else if (letter === 'enter') {
+            // TODO: handle enter logic
+        } else if (/^[a-z]$/.test(letter)) {
+            if (position >= board.length) return;
+            const newBoard = [...board];
+            newBoard[position] = letter.toUpperCase();
+            dispatch(setBoard(newBoard));
+            dispatch(incPos());
+        }
     };
+
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => handleKeyInput(e.key);
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, [board, position]);
+
     return (
         <div className='keyboard-container'>
-            {rows.map((row, idx) => {
-                return (
-                    <div className='row' key={idx}>
-                        {idx === 2 && <span className='letter-row'>Enter</span>}
-                        {row.split(' ').map((letter, idx) => {
-                            return (
-                                <div className='letter-row' key={idx}>
-                                    <Key letter={letter.toUpperCase()} />
-                                    {letter === 'm' && (
-                                        <span onClick={clickBack}> Back </span>
-                                    )}
-                                </div>
-                            );
-                        })}
-                    </div>
-                );
-            })}
+            {rows.map((row, idx) => (
+                <div className='row' key={idx}>
+                    {idx === 2 && (
+                        <span
+                            className='letter-row'
+                            onClick={() => handleKeyInput('Enter')}
+                        >
+                            Enter
+                        </span>
+                    )}
+                    {row.split(' ').map((letter, idx2) => (
+                        <div className='letter-row' key={idx2}>
+                            <Key
+                                letter={letter.toUpperCase()}
+                                onClick={handleKeyInput}
+                            />
+                            {letter === 'm' && (
+                                <span
+                                    onClick={() => handleKeyInput('Backspace')}
+                                >
+                                    Back
+                                </span>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
     );
 };
+
 export default Keyboard;
